cbb9a703a288dce80e3ea5b571a21761dc1806c0
Miguel Castro <69170175+Miguel-de-Castro@users.noreply.github.com>
2023-09-21 20:06:41 -0300

    Update sum_aulasor.c

--- sum_aulasor.c
+++ sum_aulasor.c
@@ -16,18 +16,18 @@
 void sum(char* output, const long unsigned int d, const long unsigned int n) {
     long unsigned int digit, i, remainder, div, mod;
     long unsigned int digits[d + 11];
-//pragma omp parallel for
+#pragma omp parallel for
     for (digit = 0; digit < d + 11; ++digit) {
         digits[digit] = 0;
     }
 
-//pragma omp parallel for private(digit, i,remainder, div, mod)
+#pragma omp parallel for private(digit, i,remainder, div, mod)
     for (i = 1; i <= n; ++i) {
         remainder = 1;
         for (digit = 0; digit < d + 11 && remainder; ++digit) {
             div = remainder / i;
             mod = remainder % i;
-//pragma omp parallel atomic
+#pragma omp parallel atomic
             digits[digit] += div;
             remainder = mod * 10;
         }
